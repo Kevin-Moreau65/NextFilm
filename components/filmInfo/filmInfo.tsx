0089b0926faa1bc807620ff6b1ccae6e55f48640
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2021-10-09 21:10:31 +0200

    InfoFilm 4/4

--- components/filmInfo/filmInfo.tsx
+++ components/filmInfo/filmInfo.tsx
@@ -115,6 +115,36 @@ export default class GetInfo extends Component<{ content: FilmOrSerie, back: any
             case "Genres":
                 element = <div className={styles.divGenres}>{this.props.content.genres.map((content: Genre, index: number) => (Splitand(content.name, index)))}</div>
                 return element
+            case "Info":
+                if (this.props.content.name !== undefined) {
+                    element = <div className={styles.divInfo}><h2>Type</h2>
+                        <h3>Série</h3>
+                        <h2>Titre d&apos;origine</h2>
+                        <h3 style={Titlesize(this.props.content.original_name)}>{this.props.content.original_name}</h3>
+                        <h2>Date de sortie</h2>
+                        <h3>{this.props.content.first_air_date}</h3>
+                        <h2>Note</h2>
+                        <h3 style={NoteColor(this.props.content.vote_average)}>{this.props.content.vote_average}/10</h3>
+                        <h3>Noté par {this.props.content.vote_count} personne</h3>
+                        <h2>Durée</h2>
+                        <h3>Nombre d&apos;épisode : {this.props.content.number_of_episodes}</h3>
+                        <h3>Durée par episode : {this.props.content.episode_run_time[0]} min</h3>
+                        <h3>Saisons : {this.props.content.number_of_seasons}</h3></div>
+                    return element
+                } else {
+                    element = <div className={styles.divInfo}><h2>Type</h2>
+                        <h3>Film</h3>
+                        <h2>Titre d&apos;origine</h2>
+                        <h3 style={Titlesize(this.props.content.original_title)}>{this.props.content.original_title}</h3>
+                        <h2>Date de sortie</h2>
+                        <h3>{this.props.content.release_date}</h3>
+                        <h2>Note</h2>
+                        <h3 style={NoteColor(this.props.content.vote_average)}>{this.props.content.vote_average}/10</h3>
+                        <h3>Noté par {this.props.content.vote_count} personne</h3>
+                        <h2>Durée</h2>
+                        <h3>{DureeH(this.props.content.runtime)}</h3></div>
+                    return element
+                }
         }
     }
     render() {
@@ -135,12 +165,12 @@ export default class GetInfo extends Component<{ content: FilmOrSerie, back: any
                             <div className={styles.divButton}>
                                 <h3 onClick={() => { this.changeDisplay("Résumé") }} className={this.state.display === "Résumé" ? styles.selected : undefined}>Résumé</h3>
                                 <h3 onClick={() => { this.changeDisplay("Genres") }} className={this.state.display === "Genres" ? styles.selected : undefined}>Genres</h3>
-                                <h3>Info</h3>
+                                <h3 onClick={() => { this.changeDisplay("Info") }} className={this.state.display === "Info" ? styles.selected : undefined}>Info</h3>
                             </div>
                         </div>
                         {this.display()}
                     </div>
-                    <div className={styles.back}><h3 onClick={this.props.back}>{"<"}</h3></div>
+                    <div className={styles.backBTN}><h3 onClick={this.props.back}>{"<"}</h3></div>
                 </Fragment>
             </Mobile>
         </Fragment>)
