4cbcb94f7066dd974a54eb359cec2cad95630a26
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2021-10-09 21:29:38 +0200

    Update function.tsx

--- components/filmInfo/function.tsx
+++ components/filmInfo/function.tsx
@@ -67,7 +67,7 @@ function GenreColor(Genre: string) {
         case "Guerre":
             return { color: "rgb(105, 192, 11)" }
         case "Musique":
-            return { backgroundColor: "white", borderRadius: "2px" }
+            return { backgroundColor: "white", borderRadius: "2px", color: "rgb(0, 0, 0)" }
         case "Mystère":
             return { color: "rgb(22, 181, 115)" }
     }
